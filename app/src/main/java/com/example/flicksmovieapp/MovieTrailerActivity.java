f1bb6c8523d03562dbbcbfb59a527285c1c3009c
elizabethdumervil@gmail.com <fRIMOMUSSES12>
2019-08-05 13:27:46 -0400

    release date and release language

---
+++ app/src/main/java/com/example/flicksmovieapp/MovieTrailerActivity.java
@@ -0,0 +1,13 @@
+package com.example.flicksmovieapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MovieTrailerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_trailer);
+    }
+}
